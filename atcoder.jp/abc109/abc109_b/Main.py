8c55a6fb8e4d06e194c578caed4fccfd0830d728
sin471 <tachiken.0414.ken@gmail.com>
2022-01-08 08:51:20 +0000

    [AC] abc109 abc109_b

---
+++ atcoder.jp/abc109/abc109_b/Main.py
@@ -0,0 +1,15 @@
+n = int(input())
+w = [input() for _ in range(n)]
+
+if len(set(w)) != n:
+    print("No")
+    exit()
+
+
+for i in range(1, n):
+    if w[i-1][-1] == w[i][0]:
+        continue
+    else:
+        print("No")
+        exit()
+print("Yes")
